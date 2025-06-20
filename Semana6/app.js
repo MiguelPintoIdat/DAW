aa47633b377ab60f7e4f29ca126931504f4f3a31
Miguel Pinto <fmpinto@idat.pe>
2025-06-19 21:52:52 -0500

    Funciones Jquery

--- Semana6/app.js
+++ Semana6/app.js
@@ -20,3 +20,38 @@ function ObtenerValor(){
 
 
 }
+
+function CambiarEstilos(){
+
+    $("#Contenedor").css({
+        "background-color": "green",
+        "width": "500px",
+        "height": "500px"
+    });
+}
+
+function CambiarRojo(){
+    $("#Contenedor").removeClass('ContenedorAzul')
+    $("#Contenedor").addClass('ContenedorRojo')
+}
+
+function CambiarAzul(){
+    $("#Contenedor").removeClass('ContenedorRojo')
+    $("#Contenedor").addClass('ContenedorAzul')
+}
+
+function AlternarClases(){
+    
+    var claseActual = $("#Contenedor").attr('class')
+
+    if(claseActual.includes("ContenedorRojo")){
+        $("#Contenedor").removeClass('ContenedorRojo')
+        $("#Contenedor").addClass('ContenedorAzul')
+    }
+    else if(claseActual.includes("ContenedorAzul"))
+    {
+        $("#Contenedor").removeClass('ContenedorAzul')
+        $("#Contenedor").addClass('ContenedorRojo')
+    }
+
+}
